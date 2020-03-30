ca53176b23a0792a833d766357493601095847f9
captain <xsxjlzy@163.com>
2020-03-30 18:31:21 +0800

    初始化代码库

---
+++ lib/views/myHome.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // how it looks.
+
+  // always marked "final".
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _topTitle = "Flutter Demo";
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$_topTitle'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            OutlineButton(
+                child: Text("登录"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login', arguments: {'state':'login'});
+                }),
+            RaisedButton(
+                child: Text("注册"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/regist');
+                }),
+            OutlineButton(
+                child: Text("切换卡"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/tabBar');
+                })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
