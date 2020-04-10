3f214191bfe5501e68374272896c295ee45aad55
captain <xsxjlzy@163.com>
2020-04-10 18:34:24 +0800

    完善布局模块内容示例

--- lib/wedgets/tabs/layout.dart
+++ lib/wedgets/tabs/layout.dart
@@ -5,7 +5,8 @@ class LayoutPage extends StatefulWidget {
   _LayoutPageState createState() => _LayoutPageState();
 }
 
-class _LayoutPageState extends State<LayoutPage> with SingleTickerProviderStateMixin {
+class _LayoutPageState extends State<LayoutPage>
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
   @override
@@ -19,53 +20,105 @@ class _LayoutPageState extends State<LayoutPage> with SingleTickerProviderStateM
     _controller.dispose();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    Widget avatar = Image.network("https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",width: 60.0,height: 60.0,fit: BoxFit.cover,);
+    Widget avatar = Image.network(
+      "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+      width: 60.0,
+      height: 60.0,
+      fit: BoxFit.cover,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text("本页展示布局"),
       ),
       body: Container(
+        // 类似于html中的div
+        // padding: EdgeInsets.all(15.0),
+        // width: double.infinity, // 设置宽度尽可能撑满父容器的办法
+        // height: 500.0,
         child: Column(
+          // 纵向布局
+          // mainAxisAlignment: MainAxisAlignment.center, // 竖向排列方式，start,center,end,spaceAround,spaceBetween,spaceEvenly(元素之间和距离父容器距离相等)
+          // crossAxisAlignment: CrossAxisAlignment.center, // 横向排列方式，start,center,end,stretch
           children: <Widget>[
-            ClipOval(
-              child: avatar,
-            ),
             Container(
-              color: Colors.red,
-              child: Row(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Text("第1行第1列第1行文本，",style: TextStyle(color: Colors.white),),
-                      Text("第1行第1列第2行文本，",style: TextStyle(color: Colors.white),),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text("第1行第2列第1行文本，"),
-                      Text("第1行第2列第2行文本，"),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                height: 100.0,
+                color: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceEvenly, // 横向排列方式，start,center,end,spaceAround,spaceBetween,spaceEvenly(元素之间和距离父容器距离相等)
+                  // crossAxisAlignment: CrossAxisAlignment.center, // 纵向排列方式，start,center,end,stretch
+                  children: <Widget>[
+                    ClipOval(
+                      child: avatar,
+                    ),
+                    ClipOval(
+                      child: avatar,
+                    ),
+                  ],
+                )),
             Container(
-              color: Colors.blue,
-              child: Row(
+              color: Colors.black12,
+              child: Column(
                 children: <Widget>[
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text("第2行第1列第1行文本，"),
-                      Text("第2行第1列第2行文本，"),
+                      Expanded(
+                        child: Container(
+                          height: 60.0,
+                          alignment: Alignment.center,
+                          color: Colors.red,
+                          child: Text("菜单1"),
+                        )
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 60.0,
+                          alignment: Alignment.center,
+                          color: Colors.blue,
+                          child: Text("菜单2"),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 60.0,
+                          alignment: Alignment.center,
+                          color: Colors.red,
+                          child: Text("菜单3"),
+                        ),
+                      )
                     ],
                   ),
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text("第2行第2列第1行文本，",style: TextStyle(color: Colors.white),),
-                      Text("第2行第2列第2行文本，",style: TextStyle(color: Colors.white),),
+                      Expanded(
+                          child: Container(
+                            height: 60.0,
+                            alignment: Alignment.center,
+                            color: Colors.blue,
+                            child: Text("菜单1"),
+                          )
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 60.0,
+                          alignment: Alignment.center,
+                          color: Colors.red,
+                          child: Text("菜单2"),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 60.0,
+                          alignment: Alignment.center,
+                          color: Colors.blue,
+                          child: Text("菜单3"),
+                        ),
+                      )
                     ],
                   ),
                 ],
@@ -73,10 +126,9 @@ class _LayoutPageState extends State<LayoutPage> with SingleTickerProviderStateM
             ),
             RaisedButton(
                 child: Text("路由"),
-                onPressed: (){
-                    Navigator.pushNamed(context, '/page1');
-                }
-            )
+                onPressed: () {
+                  Navigator.pushNamed(context, '/page1');
+                }),
           ],
         ),
       ),
